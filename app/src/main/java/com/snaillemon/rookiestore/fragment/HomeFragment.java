4ef36176d911983ad3d95264c71a761c7cc1a5a2
SnailLemon <hyy8516998@163.com>
2016-09-24 10:53:31 +0800

    commit first

--- app/src/main/java/com/snaillemon/rookiestore/fragment/HomeFragment.java
+++ app/src/main/java/com/snaillemon/rookiestore/fragment/HomeFragment.java
@@ -1,7 +1,7 @@
 package com.snaillemon.rookiestore.fragment;
 
-import android.media.MediaCodecList;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,13 +13,15 @@ import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.snaillemon.rookiestore.R;
 import com.snaillemon.rookiestore.bean.Banner;
+import com.snaillemon.rookiestore.bean.Contants;
+import com.snaillemon.rookiestore.http.BaseCallback;
 import com.snaillemon.rookiestore.http.OkHttpHelper;
 import com.snaillemon.rookiestore.http.SpotsCallback;
+import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import java.io.IOException;
 import java.util.List;
-
-import static android.os.Build.VERSION_CODES.M;
 
 /**
  * Created by GoodBoy on 2016/9/19.
@@ -27,6 +29,8 @@ import static android.os.Build.VERSION_CODES.M;
 public class HomeFragment extends BaseFragment{
     @ViewInject(R.id.slider)
     private SliderLayout mSliderLayout;
+    @ViewInject(R.id.home_recyclerview)
+    private RecyclerView mRecyclerView;
     private List<Banner> mBanners;
     private OkHttpHelper httpHelper = OkHttpHelper.getInstance();
     @Override
@@ -39,8 +43,38 @@ public class HomeFragment extends BaseFragment{
         initRecyclerView();
     }
     private void initRecyclerView() {
+        httpHelper.get(Contants.API.CAMPAIGN_HOME, new BaseCallback() {
+            @Override
+            public void onRequestBefore(Request request) {
+
+            }
+
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onRepose(Response response) {
+
+            }
+
+            @Override
+            public void onSuccess(Response response, Object o) {
+                initData();
+            }
+
+            @Override
+            public void onError(Response response, int code, Exception e) {
+
+            }
+        });
+    }
+
+    private void initData() {
 
     }
+
     private void requestImages() {
         String url ="http://112.124.22.238:8081/course_api/banner/query?type=1";
         httpHelper.get(url, new SpotsCallback<List<Banner>>(getContext()) {
@@ -51,7 +85,7 @@ public class HomeFragment extends BaseFragment{
             }
             @Override
             public void onError(Response response, int code, Exception e) {
-
+                //picture loading error
             }
         });
     }
